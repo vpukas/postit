9c26ffd4b9dda8a61ebbd19b9b113ac8f2b13961
vpukas <valera.pukas14@gmail.com>
2023-04-09 00:56:45 +0200

    Add: project initialisation

---
+++ src/main/java/com/vpukas/postit/PostItApplication.java
@@ -0,0 +1,13 @@
+package com.vpukas.postit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vpukas/postit/PostItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpukas.postit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
